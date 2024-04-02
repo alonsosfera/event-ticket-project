64c04bcb596db2944fb60d4fc516b7eaa57a11c6
Alonso Gutierrez <alonso.gtb@gmail.com>
2024-04-02 13:24:00 -0600

    Fix eslint warnings

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import Head from "next/head"
+import Image from "next/image"
+import { Inter } from "next/font/google"
+import styles from "@/styles/Home.module.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
@@ -24,8 +24,7 @@ export default function Home() {
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
-              rel="noopener noreferrer"
-            >
+              rel="noopener noreferrer">
               By{" "}
               <Image
                 src="/vercel.svg"
@@ -33,8 +32,7 @@ export default function Home() {
                 className={styles.vercelLogo}
                 width={100}
                 height={24}
-                priority
-              />
+                priority />
             </a>
           </div>
         </div>
@@ -46,8 +44,7 @@ export default function Home() {
             alt="Next.js Logo"
             width={180}
             height={37}
-            priority
-          />
+            priority />
         </div>
 
         <div className={styles.grid}>
@@ -55,8 +52,7 @@ export default function Home() {
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <h2>
               Docs <span>-&gt;</span>
             </h2>
@@ -69,8 +65,7 @@ export default function Home() {
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <h2>
               Learn <span>-&gt;</span>
             </h2>
@@ -83,8 +78,7 @@ export default function Home() {
             href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <h2>
               Templates <span>-&gt;</span>
             </h2>
@@ -97,8 +91,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             <h2>
               Deploy <span>-&gt;</span>
             </h2>
@@ -110,5 +103,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  );
+  )
 }
