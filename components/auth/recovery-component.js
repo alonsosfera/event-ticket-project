14801863448f0679d8c91bcda355e1152aaf1518
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-08-09 12:54:49 -0600

    new styles for modal

--- components/auth/recovery-component.js
+++ components/auth/recovery-component.js
@@ -12,7 +12,17 @@ const Recovery = () => {
 
   return (
     <Modal
-      title="Recuperación de contraseña"
+      className="modal-update-password"
+      title={
+        <div style={{
+          width: "100%",
+          borderBottom: "1px solid #000",
+          paddingBottom: "8px",
+          padding: "1rem"
+        }}>
+          Recuperación de contraseña
+        </div>
+      }
       visible={visible}
       centered
       onCancel={handleCancel}
@@ -20,6 +30,7 @@ const Recovery = () => {
       width={412}
       style={{ top: 20 }}>
       <Form
+        style={{ padding:"1rem" }}
         layout="vertical"
         initialValues={{ remember: true }}
         autoComplete="off"
@@ -27,22 +38,18 @@ const Recovery = () => {
         <Form.Item
           label="Nueva contraseña"
           name="new_password"
-          rules={[
-            { message: "Por favor introduce tu nueva contraseña!" }
-          ]}>
+          rules={[{ message: "Por favor introduce tu nueva contraseña!" }]}>
           <Input.Password />
         </Form.Item>
 
         <Form.Item
           label="Confirmar contraseña"
           name="password_confirmation"
-          rules={[
-            { message: "Por favor confirma tu contraseña!" }
-          ]}>
+          rules={[{ message: "Por favor confirma tu contraseña!" }]}>
           <Input.Password />
         </Form.Item>
 
-        <Form.Item>
+        <Form.Item style={{ marginBottom: "0" }}>
           <div style={{ display: "flex", justifyContent: "flex-end", gap: "10px" }}>
             <Button type="default" onClick={handleCancel}>
               Cancelar
