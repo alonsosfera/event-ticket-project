bc0307e3d1454618bfba47f4a1a8c0a18781968f
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-08-15 10:45:31 -0600

    home page

--- components/home-component.js
+++ components/home-component.js
@@ -1,98 +1,32 @@
-import Image from "next/image"
-import { Inter } from "next/font/google"
+import { Row, Col } from "antd"
 
-const inter = Inter({ subsets: ["latin"] })
 
 const HomeComponent = () => {
   return (
-    <main className={`home main ${inter.className}`}>
-      <div className={"description"}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={"code"}>pages/index.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer">
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={"vercelLogo"}
-              width={100}
-              height={24}
-              priority />
-          </a>
-        </div>
-      </div>
-
-      <div className={"center"}>
-        <Image
-          className={"logo"}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority />
-      </div>
-
-      <div className={"grid"}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={"card"}
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>
-            Find in-depth information about Next.js features and&nbsp;API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={"card"}
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={"card"}
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={"card"}
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL
-            with&nbsp;Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Row  style={{ background: "white", padding: "12px" }} gutter={[40, 24]}>
+      <Col span={24}>
+        <Row gutter={[24,24]}>
+          <Col span={6} style={{ background: "blue" }}>Proximo</Col>
+          <Col span={6} style={{ background: "blue" }}>Proximo</Col>
+          <Col span={6} style={{ background: "blue" }}>Proximo</Col>
+          <Col span={6} style={{ background: "blue" }}>Proximo</Col>
+        </Row>
+      </Col>
+      <Col span={24}>
+        <Row gutter={16}>
+          <Col span={6} style={{ background: "blue" }}>Invitación</Col>
+          <Col span={18} style={{ background: "red" }}>Invitación</Col>
+        </Row>
+      </Col>
+      <Col span={24}>
+        <Row gutter={16}>
+          <Col span={6} style={{ background: "red" }}>Proximo</Col>
+          <Col span={6} style={{ background: "red" }}>Proximo</Col>
+          <Col span={6} style={{ background: "red" }}>Proximo</Col>
+          <Col span={6} style={{ background: "red" }}>Proximo</Col>
+        </Row>
+      </Col>
+    </Row>
   )
 }
 
