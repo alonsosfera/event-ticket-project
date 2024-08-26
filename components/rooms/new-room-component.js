88ee7273e37d69fc832dce3c9653891b95f60bce
Alex Armendariz <115047842+alexarmendariz03@users.noreply.github.com>
2024-08-26 09:08:51 -0600

    new styles for the modal, add the input to load images

--- components/rooms/new-room-component.js
+++ components/rooms/new-room-component.js
@@ -1,16 +1,44 @@
-import { Form , Input } from "antd"
+import { Form, Input } from "antd"
 import Dragger from "antd/es/upload/Dragger"
 import { InboxOutlined } from "@ant-design/icons"
+import { useState } from "react"
 
+const NewRoom = ({  onSubmit }) => {
+  const [fileList, setFileList] = useState([
+    {
+      uid: "-1",
+      name: "File.png",
+      status: "done",
+      url: "http://www.baidu.com/file.png"
+    }
+  ])
 
-const NewRoom = ({ onCancel, onSubmit }) => {
+  const handleChange = info => {
+    let newFileList = [...info.fileList]
+    newFileList = newFileList.slice(-2) // Limitar a 2 archivos recientes
+    newFileList = newFileList.map(file => {
+      if (file.response) {
+        file.url = file.response.url
+      }
+      return file
+    })
+    setFileList(newFileList)
+  }
 
-  return(
+  const uploadProps = {
+    action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
+    onChange: handleChange,
+    multiple: true,
+    fileList
+  }
+
+  return (
     <Form
       layout="vertical"
       initialValues={{ remember: true }}
       autoComplete="off"
       onFinish={onSubmit}>
+
       <Form.Item
         label="Nombre"
         name="name"
@@ -28,19 +56,17 @@ const NewRoom = ({ onCancel, onSubmit }) => {
       <Form.Item
         label="Capacidad"
         name="capacity"
-        rules={[{ message: "Por favor confirma la capacidad del salon" }]}>
+        rules={[{ message: "Por favor confirma la capacidad del salón" }]}>
         <Input />
       </Form.Item>
 
-      <Form.Item
-        label="Capacidad"
-        name="capacity"
-        rules={[{ message: "Por favor confirma la capacidad del salon" }]}>
-        <Dragger>
+      {/* Upload Dragger */}
+      <Form.Item label="Sube tus archivos">
+        <Dragger {...uploadProps}>
           <p className="ant-upload-drag-icon">
             <InboxOutlined style={{ color: "#2F333C" }} />
           </p>
-          <p className="ant-upload-text">Click o arrastra aqui tu archivo</p>
+          <p className="ant-upload-text">Haz clic o arrastra aquí tu archivo</p>
         </Dragger>
       </Form.Item>
     </Form>
