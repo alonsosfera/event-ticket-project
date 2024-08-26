a3a75063344fbc69199f014494363f6881f22b71
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-08-26 15:43:35 -0600

    new component RoomsList displays data in a table in rooms on xs screens

---
+++ components/rooms/rooms-list-component.js
@@ -0,0 +1,21 @@
+import { Descriptions } from "antd"
+
+const RoomsListComponent = ({ dataSource, columns }) => {
+  return (
+    <div>
+      {dataSource.map((data, index) => (
+        <Descriptions key={data.key} title={`Salón ${index + 1}`}>
+          {columns.map(column => (
+            column.dataIndex && (
+              <Descriptions.Item key={column.key} label={column.title}>
+                {data[column.dataIndex]}
+              </Descriptions.Item>
+            )
+          ))}
+        </Descriptions>
+      ))}
+    </div>
+  )
+}
+
+export default RoomsListComponent
