d9d2fe20a8362e576a2c4b47002faa01c192a893
Arturo <turriscontreras@gmail.com>
2024-08-28 17:42:37 -0600

    Se cambio el ajuste responsivo de las card en event-card-component, se cambio space por Row y Col

--- components/events/event-card-component.js
+++ components/events/event-card-component.js
@@ -1,4 +1,4 @@
-import { Card, Button, Space } from "antd"
+import { Card, Button, Row, Col } from "antd"
 import React from "react"
 import { useEvent } from "./event-context"
 
@@ -8,38 +8,39 @@ const EventCard = () => {
 
   return (
     <>
-      <Space
-        className="card-space"
-        direction="horizontal"
-        size={16}
-        wrap>
+      <Row gutter={[16,6]}>
         {eventData.map((event, index) => {
           const cardClass = `card-container card-color-${(index % 4) + 1}`
           return (
-            <Card
+            <Col
               key={index}
-              className={cardClass}
-              title={<span title={event.title} className="card-title">{event.title}</span>}
-              extra={
-                <Button
-                  type="link"
-                  className="card-extra"
-                  onClick={() => setSelectedEvent(event)}>
-                  Ver detalles
-                </Button>
-              }>
-              <div className="card-content">
-                <p>{event.date}</p>
-                <p>{event.location}</p>
-              </div>
-              <div className="card-invites">
-                <span>{event.totalInvites} invitados</span>
-                <span>{event.remainingInvites} invitados restantes</span>
-              </div>
-            </Card>
+              xs={24} sm={24}
+              md={12} lg={12}
+              xl={6}>
+              <Card
+                className={cardClass}
+                title={<span title={event.title} className="card-title">{event.title}</span>}
+                extra={
+                  <Button
+                    type="link"
+                    className="card-extra"
+                    onClick={() => setSelectedEvent(event)}>
+                    Ver detalles
+                  </Button>
+                }>
+                <div className="card-content">
+                  <p>{event.date}</p>
+                  <p>{event.location}</p>
+                </div>
+                <div className="card-invites">
+                  <span>{event.totalInvites} invitados</span>
+                  <span>{event.remainingInvites} invitados restantes</span>
+                </div>
+              </Card>
+            </Col>
           )
         })}
-      </Space>
+      </Row>
     </>
   )
 
