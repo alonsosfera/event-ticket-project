30d9a82a6a591ac7a7601056314009c59080adb7
Arturo <turriscontreras@gmail.com>
2024-08-29 11:17:09 -0600

    Se quito el bóton de ver detalles en eventCard y ahora toda la tarjeta ejecuta la acción de ver el evento.

--- components/events/event-card-component.js
+++ components/events/event-card-component.js
@@ -1,4 +1,4 @@
-import { Card, Button, Row, Col } from "antd"
+import { Card, Row, Col } from "antd"
 import React from "react"
 import { useEvent } from "./event-context"
 
@@ -8,38 +8,32 @@ const EventCard = () => {
 
   return (
     <>
-      <Row gutter={[16,6]}>
+      <Row justify={"center"} gutter={[16, 6]}>
         {eventData.map((event, index) => {
-          const cardClass = `card-container card-color-${(index % 4) + 1}`
-          return (
-            <Col
-              key={index}
-              xs={24} sm={24}
-              md={12} lg={12}
-              xl={6}>
-              <Card
-                className={cardClass}
-                title={<span title={event.title} className="card-title">{event.title}</span>}
-                extra={
-                  <Button
-                    type="link"
-                    className="card-extra"
-                    onClick={() => setSelectedEvent(event)}>
-                    Ver detalles
-                  </Button>
-                }>
-                <div className="card-content">
-                  <p>{event.date}</p>
-                  <p>{event.location}</p>
-                </div>
-                <div className="card-invites">
-                  <span>{event.totalInvites} invitados</span>
-                  <span>{event.remainingInvites} invitados restantes</span>
-                </div>
-              </Card>
-            </Col>
-          )
-        })}
+        const cardClass = `card-container card-color-${(index % 4) + 1}`
+        return (
+          <Col
+            key={index}
+            xs={24} sm={24}
+            md={12} lg={12}
+            xl={6}>
+            <Card
+              className={cardClass}
+              title={<span title={event.title} className="card-title">{event.title}</span>}
+              onClick={() => setSelectedEvent(event)}
+              hoverable>
+              <div className="card-content">
+                <p>{event.date}</p>
+                <p>{event.location}</p>
+              </div>
+              <div className="card-invites">
+                <span>{event.totalInvites} invitados</span>
+                <span>{event.remainingInvites} invitados restantes</span>
+              </div>
+            </Card>
+          </Col>
+        )
+      })}
       </Row>
     </>
   )
