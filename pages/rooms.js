0485e8e0717aaf561ad0b37eed6c37b8dd6bd8a1
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-09-02 12:24:48 -0600

    [PP-28] added the layout component to the rooms page, also added the menu navigation for the rooms page.

--- pages/rooms.js
+++ pages/rooms.js
@@ -1,11 +1,12 @@
 import Rooms from "@/components/rooms/rooms-component"
+import Layout from "@/components/layout/layout-component"
 
 
 function RoomsPage() {
   return (
-    <>
+    <Layout>
       <Rooms />
-    </>
+    </Layout>
   )
 }
 
