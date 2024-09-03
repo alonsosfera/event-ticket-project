309ad8172484fc48f81062fa1e96487349f80734
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-09-03 11:11:33 -0600

    [PP-33] add title and button to add new room map

--- components/room-maps/room-maps-components.js
+++ components/room-maps/room-maps-components.js
@@ -1,8 +1,51 @@
-
+import { Button, Col, Row, Typography, Modal } from "antd"
+import { useState } from "react"
 
 const RoomMaps = () => {
-  return(
-    <h1>Rooms Aqui </h1>
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+
+  return (
+    <>
+      <Row style={{ padding: "20px" }} gutter={[24, 0]}>
+        <Col span={24} style={{ marginBottom: "10px" }}>
+          <Row justify="space-between" align="middle">
+            <Col>
+              <Typography.Title level={2} style={{ color: "#2F333C" }}>
+                Usuarios
+              </Typography.Title>
+            </Col>
+            <Col
+              xs={12} md={4}
+              lg={4}>
+              <Button
+                key="submit"
+                onClick={showModal}
+                style={{ backgroundColor: "#2F333C", color: "#fff" }}>
+                Agregar nuevo acomodo
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+
+      <Modal
+        title="Nuevo Usuario"
+        open={isModalVisible}
+        onCancel={handleCancel}
+        cancelText="Cancelar"
+        okText="Guardar"
+        width={381}>
+        Modal Acomodo
+      </Modal>
+    </>
   )
 }
 
