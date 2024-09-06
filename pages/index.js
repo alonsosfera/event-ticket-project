74927f37abc8b554bf2ec8f9601c0fa4a86a3002
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-09-06 09:37:29 -0600

    [PP-34] adjusted the mobile view of the home-component component, also called the layout to wrap the home component page.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import HomeComponent from "@/components/host/home-component"
+import Layout from "@/components/layout/layout-component"
 
 export default function Home() {
   return (
@@ -10,7 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HomeComponent />
+      <Layout>
+        <HomeComponent />
+      </Layout>
     </>
   )
 }
