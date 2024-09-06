b0602b9972608ae0819933b05d6b04785629cedf
Alonso Gutierrez <alonso.gtb@gmail.com>
2024-09-06 14:33:07 -0600

    [Auth] Update UsersPage to implement pageAuth

--- pages/users.js
+++ pages/users.js
@@ -1,10 +1,13 @@
+import { pageAuth } from "@/helpers/page-auth"
+import UserRoleEnum from "@/models/user-role-enum"
 import Users from "@/components/user/users-component"
-
 
 export default function UsersPage() {
   return(
-    <div>
-      <Users />
-    </div>
+    <Users />
   )
+}
+
+export async function getServerSideProps(context) {
+  return pageAuth(context, [UserRoleEnum.OWNER, UserRoleEnum.ADMIN])
 }
