62bf59903661494b9f446be4020a7147b2a11a04
Arturo <turriscontreras@gmail.com>
2024-09-09 12:07:26 -0600

    Se agrupo los Col en un Row en vez de un div.

--- components/owner/owner-events-component.js
+++ components/owner/owner-events-component.js
@@ -1,15 +1,17 @@
 import EventButtons from "./owner-events-buttons-component"
 import OwnerEventTableMobile from "./owner-events-mobile-table-component"
 import OwnerEventsTable from "./owner-events-table-component"
-import { Col } from "antd"
+import { Col, Row } from "antd"
 
 const OwnerEventsComponent = () => {
   return (
-    <div>
+    <>
       <EventButtons />
-      <Col xs={0} md={24}><OwnerEventsTable /></Col>
-      <Col xs={24} md={0}><OwnerEventTableMobile /></Col>
-    </div>
+      <Row>
+        <Col xs={0} md={24}><OwnerEventsTable /></Col>
+        <Col xs={24} md={0}><OwnerEventTableMobile /></Col>
+      </Row>
+    </>
   )
 }
 
