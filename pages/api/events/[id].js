1cd1bf0bf12169dadfdff4b1083726fea2a2546e
Arturo <turriscontreras@gmail.com>
2024-09-19 15:14:35 -0600

    Se separo en 1 archivo para obtener un evento por su id.

---
+++ pages/api/events/[id].js
@@ -0,0 +1,28 @@
+// pages/api/events/[id].js
+import { prisma } from "@/lib/prisma"
+import { withAuthApi } from "@/helpers/with-api-auth"
+
+async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Método no permitido" })
+  }
+
+  const { id } = req.query
+
+  try {
+    const event = await prisma.event.findUnique({
+      where: { id },
+      include: { eventHall: true, users: true }
+    })
+
+    if (!event) {
+      return res.status(404).json({ message: "Evento no encontrado" })
+    }
+
+    return res.status(200).json(event)
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener evento", error })
+  }
+}
+
+export default withAuthApi(handler)
