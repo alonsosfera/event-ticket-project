6f53cc26bc062afb8324c9c6d983dc247567936e
Arturo <turriscontreras@gmail.com>
2024-09-20 15:38:08 -0600

    Se borro el archivo owner-events, ahora se llama las 2 páginas en events.js pero abre una de las 2 dependiendo sul rol de usuario. Se corrigió el título de la página (Usuarios) ahora es: Eventos

--- pages/events.js
+++ pages/events.js
@@ -3,15 +3,28 @@ import EventTable from "@/components/host/events/event-table-component"
 import TableMobile from "@/components/host/events/event-table-mobile-component"
 import { EventProvider } from "@/components/events/event-context"
 import { Row, Col } from "antd"
-export default function EventsPage() {
+import OwnerEventsComponent from "@/components/owner/owner-events-component"
+import { pageAuth } from "@/helpers/page-auth"
+import UserRoleEnum from "@/models/user-role-enum"
+
+export default function EventsPage({ user }) {
+
+  const { role } = user
+
   return (
     <EventProvider>
       <Layout>
+        {role === UserRoleEnum.HOST &&
         <Row>
           <Col md={24} xs={0}><EventTable /></Col>
           <Col md={0} xs={24}><TableMobile /></Col>
-        </Row>
+        </Row>}
+        {[UserRoleEnum.OWNER, UserRoleEnum.ADMIN].includes(role) && <OwnerEventsComponent />}
       </Layout>
     </EventProvider>
   )
+}
+
+export async function getServerSideProps(context) {
+  return pageAuth(context)
 }
