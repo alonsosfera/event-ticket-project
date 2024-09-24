6068ddb4e63ef8bb64a298aa4d870a3378aa318d
Arturo <turriscontreras@gmail.com>
2024-09-24 12:25:06 -0600

    Se agrego la funcionalidad para eliminar salones por su id de la base de datos y se actuliza en redux.

--- slices/rooms-slice.js
+++ slices/rooms-slice.js
@@ -15,9 +15,24 @@ const roomsSlice = createSlice({
     setRoomsList: (state, action) => {
       state.isLoading = false
       state.list = action.payload
+    },
+    createRoom: (state, action) => {
+      state.list = [action.payload, ...state.list]
+    },
+    deleteRoom: (state, action) => {
+      state.list = state.list.filter(room => room.id !== action.payload)
+    },
+    updateRoom: (state, action) => {
+      state.list = state.list.map(room => {
+        if (room.id === action.payload.id) {
+        return action.payload
+        }
+        return room
+      })
     }
+
   }
 })
 
-export const { fetchRoomsList, setRoomsList } = roomsSlice.actions
+export const { fetchRoomsList, setRoomsList, createRoom, deleteRoom, updateRoom } = roomsSlice.actions
 export default roomsSlice.reducer
