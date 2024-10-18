60cc6417ac944e3176dc3a5ddbf3b9094dc21f68
Arturo <turriscontreras@gmail.com>
2024-10-18 14:47:49 -0600

    Se actualizaron algunos estilos para que la página quede igual a el figma, y se añadio el GET de eventos para mostrar datos reales.

--- components/host/home-component.js
+++ components/host/home-component.js
@@ -1,51 +1,76 @@
-import { Row, Col, Card, Image, Statistic, Typography } from "antd"
+import { Row, Col, Card, Image, Statistic, Typography, message } from "antd"
+import { useEffect, useCallback } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import axios from "axios"
+import { useSession } from "next-auth/react"
+import { setEventsList } from "@/slices/events-slice"
 
 const HomeComponent = () => {
-  const { Text } = Typography
+  const { Text, Title } = Typography
+  const dispatch = useDispatch()
+  const { data: session } = useSession()
+  const userId = session?.user?.id
+  const events = useSelector(state => state.eventsSlice.list)
+
+  const fetchEvents = useCallback(async () => {
+    if (userId) {
+      try {
+        const response = await axios.get("/api/events/list", {
+          params: { userId }
+        })
+        dispatch(setEventsList(response.data))
+      } catch (error) {
+        console.error("Error al traer los eventos:", error)
+        message.error("Error al obtener los eventos")
+      }
+    }
+  }, [userId, dispatch])
+
+  useEffect(() => {
+    fetchEvents()
+  }, [fetchEvents])
 
   return (
     <Row
-      className="home-component" gutter={[16, 24]}
-      justify="center" align="middle">
+      className="home-component"
+      gutter={[16, 24]}
+      justify="center"
+      align="middle">
       <Col span={24}>
         <Typography.Title level={1}>Home</Typography.Title>
         <Row justify="space-between" align="middle">
-          <Col xs={0}>
+          <Col>
             <Typography.Title level={5}>Próximo Evento: Cumple Claudia</Typography.Title>
           </Col>
-          <Col xs={0}>
+          <Col>
             <Text type="secondary">28/04/2024</Text>
           </Col>
         </Row>
 
         <Row gutter={[16, 24]} justify="center">
           <Col
-            xs={12}
-            sm={12}
+            xs={12} sm={12}
             md={6}>
             <Card>
               <Statistic title="Invitaciones enviadas" value={50} />
             </Card>
           </Col>
           <Col
-            xs={12}
-            sm={12}
+            xs={12} sm={12}
             md={6}>
             <Card>
               <Statistic title="Invitaciones por enviar" value={103} />
             </Card>
           </Col>
           <Col
-            xs={0}
-            sm={12}
+            xs={0} sm={12}
             md={6}>
             <Card>
               <Statistic title="Invitados" value={80} />
             </Card>
           </Col>
           <Col
-            xs={0}
-            sm={12}
+            xs={0} sm={12}
             md={6}>
             <Card>
               <Statistic title="Invitados restantes" value={150} />
@@ -59,7 +84,7 @@ const HomeComponent = () => {
           <Col
             xs={24} md={12}
             lg={6}>
-            <Card>
+            <Card className="card-color">
               <Typography.Title level={5}>Invitación</Typography.Title>
               <Text type="secondary">Actualizada hace 2 días</Text>
               <Image src="/party-invitation.png" alt="Party Invitation" />
@@ -68,7 +93,7 @@ const HomeComponent = () => {
           <Col
             xs={24} md={12}
             lg={18}>
-            <Card>
+            <Card className="card-color">
               <Typography.Title level={5}>Acomodo de invitados</Typography.Title>
               <Text type="secondary">Actualizada hace 2 días</Text>
             </Card>
@@ -77,40 +102,19 @@ const HomeComponent = () => {
       </Col>
 
       <Col span={24} className="no-border-col">
-        <Typography.Title level={5}>Próximos Eventos</Typography.Title>
+        <Title level={5}>Próximos Eventos</Title>
         <Row gutter={[16, 24]} justify="center">
-          <Col
-            xs={24}
-            sm={12}
-            md={6}>
-            <Card>
-              <Statistic title="14/04/2024" value={"50 Años Martha"} />
-            </Card>
-          </Col>
-          <Col
-            xs={0}
-            sm={12}
-            md={6}>
-            <Card>
-              <Statistic title="14/04/2024" value={"Xv Carla"} />
-            </Card>
-          </Col>
-          <Col
-            xs={0}
-            sm={12}
-            md={6}>
-            <Card>
-              <Statistic title="14/04/2024" value={"50 Años Martha"} />
-            </Card>
-          </Col>
-          <Col
-            xs={0}
-            sm={12}
-            md={6}>
-            <Card>
-              <Statistic title="14/04/2024" value={"50 Años Martha"} />
-            </Card>
-          </Col>
+          {events?.slice(0, 4).map((event, index) => (
+            <Col
+              key={index} xs={24}
+              sm={12} md={6}>
+              <Card className="card-color">
+                <Statistic
+                  title={new Date(event.eventDate).toLocaleDateString("es-ES")}
+                  value={event.name} />
+              </Card>
+            </Col>
+    ))}
         </Row>
       </Col>
     </Row>
