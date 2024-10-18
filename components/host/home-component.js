ab634b4adc89e5e625d52e85fc14158fd8c2806b
Arturo <turriscontreras@gmail.com>
2024-10-18 16:04:30 -0600

    Optimización del código para reducir líneas.
    Se agrego los invitados restantes según la capacidad del salón menos los invitados

--- components/host/home-component.js
+++ components/host/home-component.js
@@ -17,9 +17,7 @@ const HomeComponent = () => {
   const fetchEvents = useCallback(async () => {
     if (userId) {
       try {
-        const response = await axios.get("/api/events/list", {
-          params: { userId }
-        })
+        const response = await axios.get("/api/events/list", { params: { userId } })
         dispatch(setEventsList(response.data))
       } catch (error) {
         console.error("Error al traer los eventos:", error)
@@ -42,83 +40,56 @@ const HomeComponent = () => {
     fetchEvents()
   }, [fetchEvents])
 
-  const getSortedEvents = events => {
-    const currentDate = dayjs()
-
-    const sortedEvents = [...events].sort((a, b) => {
-      const dateA = dayjs(a.eventDate)
-      const dateB = dayjs(b.eventDate)
-
-      if (dateA.isAfter(currentDate) && dateB.isBefore(currentDate)) return -1
-      if (dateA.isBefore(currentDate) && dateB.isAfter(currentDate)) return 1
-      if (dateA.isAfter(currentDate) && dateB.isAfter(currentDate)) return dateA.diff(dateB)
-      return dateB.diff(dateA)
-    })
-
-    return sortedEvents
-  }
-
-  const sortedEvents = getSortedEvents(events)
-  const firstEvent = sortedEvents[0]
-  const firstEventId = firstEvent ? firstEvent.id : null
-  const totalGuestQuantity = firstEventDetails?.guests?.reduce((acc, guest) => acc + guest.guestQuantity, 0) || 0
+  const sortedEvents = [...events].sort((a, b) => {
+    const dateA = dayjs(a.eventDate)
+    const dateB = dayjs(b.eventDate)
+    return dateA.isAfter(dayjs()) ? (dateB.isBefore(dayjs()) ? -1 : dateA.diff(dateB)) : (dateB.isAfter(dayjs()) ? 1 : dateB.diff(dateA))
+  })
 
   useEffect(() => {
-    if (firstEventId) {
-      fetchFirstEventDetails(firstEventId)
+    if (sortedEvents.length && !firstEventDetails) {
+      fetchFirstEventDetails(sortedEvents[0].id)
     }
-  }, [firstEventId])
+  }, [sortedEvents, firstEventDetails])
+
+
+  const totalGuestQuantity = firstEventDetails?.guests?.reduce((acc, guest) => acc + guest.guestQuantity, 0) || 0
+  const remainingInvitations = firstEventDetails?.eventHall?.capacity - totalGuestQuantity || 0
+
+  const renderStatisticCard = (title, value) => (
+    <Col
+      xs={12} sm={12}
+      md={6}>
+      <Card>
+        <Statistic title={title} value={value} />
+      </Card>
+    </Col>
+  )
 
   return (
     <Row
-      className="home-component"
-      gutter={[16, 24]}
-      justify="center"
-      align="middle">
+      className="home-component" gutter={[16, 24]}
+      justify="center" align="middle">
       <Col span={24}>
-        <Typography.Title level={1}>Home</Typography.Title>
+        <Title level={1}>Home</Title>
         <Row justify="space-between" align="middle">
           <Col>
-            <Typography.Title level={5}>
-              Próximo Evento: {firstEvent ? firstEvent.name : "No hay próximos eventos"}
-            </Typography.Title>
+            <Title level={5}>
+              Próximo Evento: {sortedEvents[0] ? sortedEvents[0].name : "No hay próximos eventos"}
+            </Title>
           </Col>
           <Col>
             <Text type="secondary">
-              {firstEvent ? dayjs(firstEvent.eventDate).format("DD/MM/YYYY") : ""}
+              {sortedEvents[0] ? dayjs(sortedEvents[0].eventDate).format("DD/MM/YYYY") : ""}
             </Text>
           </Col>
         </Row>
 
         <Row gutter={[16, 24]} justify="center">
-          <Col
-            xs={12} sm={12}
-            md={6}>
-            <Card>
-              <Statistic title="Invitaciones enviadas" value={50} />
-            </Card>
-          </Col>
-          <Col
-            xs={12} sm={12}
-            md={6}>
-            <Card>
-              <Statistic title="Invitaciones por enviar" value={103} />
-            </Card>
-          </Col>
-          <Col
-            xs={0} sm={12}
-            md={6}>
-            <Card>
-              <Statistic title="Invitados" value={totalGuestQuantity} />
-            </Card>
-          </Col>
-          <Col
-            xs={0} sm={12}
-            md={6}>
-            <Card>
-              <Statistic title="Invitados restantes" value={150} />
-            </Card>
-          </Col>
+          {renderStatisticCard("Invitaciones enviadas", 50)}
+          {renderStatisticCard("Invitaciones por enviar", 103)}
+          {renderStatisticCard("Invitados", totalGuestQuantity)}
+          {renderStatisticCard("Invitados restantes", remainingInvitations)}
         </Row>
       </Col>
 
@@ -128,7 +99,7 @@ const HomeComponent = () => {
             xs={24} md={12}
             lg={6}>
             <Card className="card-color">
-              <Typography.Title level={5}>Invitación</Typography.Title>
+              <Title level={5}>Invitación</Title>
               <Text type="secondary">Actualizada hace 2 días</Text>
               <Image src="/party-invitation.png" alt="Party Invitation" />
             </Card>
@@ -137,7 +108,7 @@ const HomeComponent = () => {
             xs={24} md={12}
             lg={18}>
             <Card className="card-color">
-              <Typography.Title level={5}>Acomodo de invitados</Typography.Title>
+              <Title level={5}>Acomodo de invitados</Title>
               <Text type="secondary">Actualizada hace 2 días</Text>
             </Card>
           </Col>
@@ -147,14 +118,12 @@ const HomeComponent = () => {
       <Col span={24} className="no-border-col">
         <Title level={5}>Próximos Eventos</Title>
         <Row gutter={[16, 24]} justify="center">
-          {sortedEvents?.slice(0, 4).map((event, index) => (
+          {sortedEvents.slice(0, 4).map((event, index) => (
             <Col
               key={index} xs={24}
               sm={12} md={6}>
               <Card className="card-color">
-                <Statistic
-                  title={dayjs(event.eventDate).format("DD/MM/YYYY")}
-                  value={event.name} />
+                <Statistic title={dayjs(event.eventDate).format("DD/MM/YYYY")} value={event.name} />
               </Card>
             </Col>
           ))}
