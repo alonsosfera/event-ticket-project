0daf2d4eeb0e99f6a4d9a8de01995d82f2ed89d2
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-10-24 16:00:47 -0600

    [P-74] new component invitation digital list for view invitations designs list

--- components/digital-invitation/digital-invitation-list-component.js
+++ components/digital-invitation/digital-invitation-list-component.js
@@ -1,14 +1,39 @@
-import { Col , Row } from "antd"
-
+import { Col, Row } from "antd"
+import DigitalInvitationListItem from "@/components/digital-invitation/digital-invitation-list-item-component"
 
 const DigitalInvitationListComponent = () => {
-  return(
+  const dataSource = [
+    {
+      key: "1",
+      fileUrl: "https://via.placeholder.com/150",
+      fileName: "Conferencia Tecnológica"
+    },
+    {
+      key: "2",
+      fileUrl: "https://via.placeholder.com/150",
+      fileName: "Reunión de Negocios"
+    },
+    {
+      key: "3",
+      fileUrl: "https://via.placeholder.com/150",
+      fileName: "Fiesta Anual"
+    },
+    {
+      key: "4",
+      fileUrl: "https://via.placeholder.com/150",
+      fileName: "Seminario de Marketing"
+    }
+  ]
+
+  return (
     <Row gutter={[24, 12]}>
-      <Col span={4}>
-        Lista de Diseños
-      </Col>
+      {dataSource.map(item => (
+        <Col key={item.key} span={6}>
+          <DigitalInvitationListItem item={item} />
+        </Col>
+      ))}
     </Row>
   )
- }
+}
 
- export default DigitalInvitationListComponent
+export default DigitalInvitationListComponent
